b5532f294bb924c249bffa9fe848776b261ee28a
Mark John <makerj67@gmail.com>
2022-10-02 16:22:01 +0200

    modificato l'esercizio in ottemperanza delle linee guida del docente

--- fi/esercizi/21.c
+++ fi/esercizi/21.c
@@ -5,15 +5,18 @@ perfetto visualizza 1 seguito (dopo lo spazio) dalla radice del cubo, altrimenti
 visualizza 0 seguito da un numero intero che verrà ignorato.
 */
 
-#define EPS 1e-6
+#define EPS      1e-6
+#define CUBE     '1'
+#define NOT_CUBE '0'
 
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-  int n, d;
+int main(int argc, char **argv) {
+  int n, d, r;
   float x, y;
   scanf("%d", &n);
   d = n % 9;
+  r = 0;
   if (d < 2 || d > 7) {
     x = 0;
     y = 1; /* valore qualsiasi */
@@ -22,12 +25,16 @@ int main(int argc, char** argv) {
       y = x - (x * x * x - n) / (3.0 * x * x);
       /* printf("%11.3f\t%.8f\n", y, y - x); */
     } while (y - x > EPS || x - y > EPS);
-    if (y - (int)y > EPS)
-      printf("0 0");
-    else
-      printf("1 %d", (int)y);
-  } else {
-    printf("0 0");
+    r = y;
+    y -= r;
+    if (y < 0) y *= -1.0;
+    if (y > EPS) r = 0;
   }
+  if (!r && !n)
+    printf("%c 0", CUBE);
+  else if (!r)
+    printf("%c 0", NOT_CUBE);
+  else
+    printf("%c %d", CUBE, r);
   return 0;
 }
