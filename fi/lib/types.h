4c15a689323526ccab03b26c17c02624fc07f801
Mark John <makerj67@gmail.com>
2022-11-28 21:50:03 +0100

    aggiunta libreria sui tipi elementari

---
+++ fi/lib/types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long i64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+typedef float f32;
+typedef double f64;
+typedef long double f80;
+
+#endif
